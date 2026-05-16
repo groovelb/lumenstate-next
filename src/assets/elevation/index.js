1e1b1c9357352babf8a0fc66177738c42a550a3a
groovelb <groovelb@gmail.com>
2026-05-17 00:41:35 +0900

    delete

--- src/assets/elevation/index.js
+++
@@ -1,3 +0,0 @@
-export const elevationSet1Video = '/videos/set1_moving.mp4';
-export const elevationSet2Video = '/videos/set2_moving.mp4';
-export const elevationSet3Video = '/videos/set3_moving.mp4';
